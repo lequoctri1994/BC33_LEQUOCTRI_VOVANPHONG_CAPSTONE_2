47f664b08441d6a0929b7d4d44727389520d8232
lequoctri1994 <lequoctri1994@gmail.com>
2022-09-04 00:52:29 +0700

    done detail page

--- controllers/detail.js
+++ controllers/detail.js
@@ -1,4 +1,4 @@
-function getProductDetail () {
+function getProductAPI () {
     console.log('123')
     var promise = axios ({
         url:`https://shop.cyberlearn.vn/api/Product`,
@@ -7,7 +7,9 @@ function getProductDetail () {
     });
 
     promise.then(function(result){
-        console.log(result);
+        renderProduct(result.data.content);
+        renderDetail(result.data.content);
+
     });
 
     promise.catch(function(err){
@@ -15,39 +17,78 @@ function getProductDetail () {
     });
 }
 
-function getProductDetail (arrProduct) {
+// function getProductAPI() {
+//     var promise = axios ({
+//         url:'https://shop.cyberlearn.vn/api/Product',
+//         method:'GET',
+//         reponseType:'JSON',
+//     });
+
+//     promise.then(function(result){
+//         console.log('result', result.data);
+//         renderProduct(result.data.content);
+//     });
+
+//     promise.catch(function(err) {
+//         console.log(err);
+//     });
+// }
+
+
+function renderDetail (arrProduct) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const detailId = urlParams.get('productid'); 
+    for (var i = 0; i < 6; i++) {
+        var pr = arrProduct[i];
+        if (pr.id == detailId) {
+            JSON.parse(pr.size);
     var html = '';
-    html =+ `
+    html += `
+    <div class=left>
+    <div class="product-img">
     <img src="${pr.image}" />
+    </div>
+    </div>
+    
+    
+    <div class=right>
+    <div class="product-info">
     <h1>${pr.name}</h1>
     <h2>${pr.description}</h2>
-    <span>${pr.size}</span>
-    <h2>${pr.price}</h2>
+    </div>
+
+    <div class="product-size">
+    <h3>Available size</h3>
+    <button>${(JSON.parse(pr.size))[0]}</button>
+    <button>${(JSON.parse(pr.size))[1]}</button>
+    <button>${(JSON.parse(pr.size))[2]}</button>
+    <button>${(JSON.parse(pr.size))[3]}</button>
+    <button>${(JSON.parse(pr.size))[4]}</button>
+    </div>
+
+    <div class=product-price>
+    <h2>${pr.price}$</h2>
+    </div>
+
+    <div class=product-cart>
+    <div class="count">
     <button class=plus>+</button>
-
-
+    <span id="productCount">1</span>
+    <button class=plus>-</button>
+    </div>
+    <div class=cart>
     <button class=addToCart>Add to cart</button>
+    </div>
+    </div>
+    </div>
+
     `
-
+            console.log(JSON.parse(pr.size)[0])
     document.querySelector('#detailProduct').innerHTML = html;
+        }
+    }
 }
 
-function getProductAPI() {
-    var promise = axios ({
-        url:'https://shop.cyberlearn.vn/api/Product',
-        method:'GET',
-        reponseType:'JSON',
-    });
-
-    promise.then(function(result){
-        console.log('result', result.data);
-        renderProduct(result.data.content);
-    });
-
-    promise.catch(function(err) {
-        console.log(err);
-    });
-}
 
 function renderProduct(arrProduct) {
     var html = '';
@@ -90,9 +131,48 @@ function renderProduct(arrProduct) {
 
 window.onload = function () {
     getProductAPI();
-    getProductDetail();
-    console.log(getProductDetail())
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('productid');
-    console.log('param',myParam);
+    console.log(myParam);
 }
+
+
+
+// function getProductDetail (id) {
+//     console.log('123')
+//     var promise = axios ({
+//         url:`https://shop.cyberlearn.vn/api/Product/getbyid?id=${id}`,
+//         method:'GET',
+//         reponseType:'JSON',
+//     });
+
+//     promise.then(function(result){
+//         renderProduct(result.data.content)
+//     });
+
+//     promise.catch(function(err){
+//         console.log(err);
+//     });
+// }
+
+// function renderProduct (pr) {
+//     var html = '';
+//     html += `
+//     <img src="${pr.image}" />
+//     <h1>${pr.name}</h1>
+//     <h2>${pr.description}</h2>
+//     <span>${pr.size}</span>
+//     <h2>${pr.price}</h2>
+//     <button class=plus>+</button>
+
+
+//     <button class=addToCart>Add to cart</button>
+//     `
+
+//     document.querySelector('#detailProduct').innerHTML = html;
+// }
+
+// window.onload = function () {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const myParam = urlParams.get('productid');
+//     getProductDetail(myParam);
